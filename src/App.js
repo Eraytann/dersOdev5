1d95f7974042ad86d11d806a930b3ec39a2db12a
Eray Tan <eraytan@yahoo.com>
2023-11-03 23:57:14 +0300

    dersodev5

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import AddProductComponent from "./components/AddProductComponent";
+
+function App() {
+  return (
+    <>
+      <AddProductComponent></AddProductComponent>
+    </>
+  );
+}
+
+export default App;
